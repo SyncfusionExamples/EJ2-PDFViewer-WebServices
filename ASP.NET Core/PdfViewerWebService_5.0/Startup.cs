86fd00b17cc71f1bab89b4e5ede8bbc50f6b0a4b
VijayalakshmiGSF4561 <vijayalakshmi.govindan@syncfusion.com>
2025-01-15 20:49:02 +0530

    911144: Fixed larger document load issue for IIS express

--- ASP.NET Core/PdfViewerWebService_5.0/Startup.cs
+++ ASP.NET Core/PdfViewerWebService_5.0/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +28,7 @@ namespace PDFViewerWebService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<KestrelServerOptions>(options => options.Limits.MaxRequestBodySize = 52428800);
             services.AddControllers();
             services.AddMemoryCache();
             services.AddControllers().AddNewtonsoftJson(options =>
